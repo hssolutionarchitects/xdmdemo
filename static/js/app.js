e1311e0b8d3d7299245bd827616b338944da417d
Mark C Allen <mark.allen@hootsuite.com>
2017-07-24 10:01:59 -0700

    Moved resize event into a onReady handler for the iframe so that the iframe shows once it has loaded, not when the full page has loaded.

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,16 @@ var socket = new easyXDM.Socket({
     onMessage: function(message, origin){
         // Get data from JanRain
         var info = {firstName: "Mark", lastName: "Allen", email: "mark@markcallen.com"};
-        socket.postMessage(JSON.stringify(info));
+        console.log("prepostmessage");
+        setTimeout(function() {
+          console.log("intimeout");
+          socket.postMessage(JSON.stringify(info));
+        }, 10000);
     }
 });
+
+$("#bk_iframe").ready(function (){
+    // do something once the iframe is loaded
+    resizeCrossDomainIframe('bk_iframe', 'https://marksignup.hscampaigns.com');
+});
+
